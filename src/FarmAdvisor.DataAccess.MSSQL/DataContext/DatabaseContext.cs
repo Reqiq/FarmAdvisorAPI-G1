ff4883bc470c4abf7d4b532b7dded73dc17a19d5
yosephhabtamu <yosephhabtamu5@gmail.com>
2022-12-06 13:52:10 +0300

    created Entities and relation and test

---
+++ src/FarmAdvisor.DataAccess.MSSQL/DataContext/DatabaseContext.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using FarmAdvisor.DataAccess.MSSQL.Entities;
+using System.Diagnostics;
+
+
+namespace FarmAdvisor.DataAccess.MSSQL.DataContext
+{
+    public class DatabaseContext : DbContext
+    {
+
+        public static readonly OptionsBuild Options = new OptionsBuild();
+        public class OptionsBuild
+        {
+            public OptionsBuild()
+            {
+                Settings = new AppConfiguration();
+
+                OptionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+
+                OptionsBuilder.UseSqlServer(Settings.SqlConnectionString);
+
+                DatabaseOptions = OptionsBuilder.Options;
+
+            }
+
+
+            public DbContextOptionsBuilder<DatabaseContext> OptionsBuilder { get; set; }
+            public AppConfiguration Settings { get; set; }
+            public DbContextOptions<DatabaseContext> DatabaseOptions { get; set; }
+            
+        }
+
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Farm> Farms { get; set; }
+        public DbSet<Sensor> Sensors { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Field> Fields { get; set; }
+    
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            DateTime modifiedDate = new DateTime(1900, 1, 1);
+
+            #region User
+            modelBuilder.Entity<User>().ToTable("user");
+            //Primary Key & Identity Column
+            modelBuilder.Entity<User>().HasKey(us => us.UserID);
+            modelBuilder.Entity<User>().Property(us => us.UserID).IsRequired().HasColumnName("user_id");
+            //COLUMN SETTINGS 
+            modelBuilder.Entity<User>().Property(us => us.Name).IsRequired(true).HasMaxLength(100).HasColumnName("user_name");
+            modelBuilder.Entity<User>().Property(us => us.Email).IsRequired(true).HasMaxLength(100).HasColumnName("email");
+            modelBuilder.Entity<User>().Property(us => us.Phone).IsRequired(true).HasColumnName("phone_number");
+            modelBuilder.Entity<User>().Property(us => us.AuthId).IsRequired(true).HasMaxLength(250).HasColumnName("auth_id");
+            modelBuilder.Entity<User>().HasMany<Farm>(us => us.Farms)
+                .WithMany(us => us.Users);
+            #endregion
+
+
+            #region Farm
+            modelBuilder.Entity<Farm>().ToTable("farm");
+            //Primary Key & Identity Column
+            modelBuilder.Entity<Farm>().HasKey(us => us.FarmId);
+            modelBuilder.Entity<Farm>().Property(us => us.FarmId).IsRequired().HasColumnName("farm_id");
+            //COLUMN SETTINGS 
+            modelBuilder.Entity<Farm>().Property(us => us.Name).IsRequired(true).HasMaxLength(100).HasColumnName("farm_name");
+            modelBuilder.Entity<Farm>().Property(us => us.Postcode).IsRequired(true).HasMaxLength(100).HasColumnName("postcode");
+            modelBuilder.Entity<Farm>().Property(us => us.City).IsRequired(true).HasColumnName("city");
+            modelBuilder.Entity<Farm>().Property(us => us.Country).IsRequired(true).HasMaxLength(250).HasColumnName("country");
+
+            modelBuilder.Entity<Farm>()
+                .HasMany<Notification>(us=>us.Notifications)
+                .WithOne(us=>us.Farm)
+                .HasForeignKey(us=>us.NotificationId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Farm>().HasMany<Field>(us => us.Fields)
+                .WithOne(us => us.Farm)
+                .HasForeignKey(us => us.FieldId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            #endregion
+
+
+            #region Field
+            modelBuilder.Entity<Field>().ToTable("field");
+            //Primary Key & Identity Column
+            modelBuilder.Entity<Field>().HasKey(us => us.FieldId);
+            modelBuilder.Entity<Field>().Property(us => us.FieldId).IsRequired().HasColumnName("Field_id");
+            //COLUMN SETTINGS 
+            modelBuilder.Entity<Field>().Property(us => us.Name).IsRequired(true).HasMaxLength(100).HasColumnName("Field_name");
+            modelBuilder.Entity<Field>().Property(us => us.Polygon).IsRequired(true).HasMaxLength(100).HasColumnName("polygon");
+            modelBuilder.Entity<Field>().Property(us => us.Alt).IsRequired(true).HasColumnName("altitude");
+            
+            modelBuilder.Entity<Field>().HasMany<Sensor>(us => us.Sensors)
+                .WithOne(us => us.Field)
+                .HasForeignKey(us => us.SensorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+            #endregion
+
+
+            #region Sensor
+            modelBuilder.Entity<Sensor>().ToTable("sensor");
+            //Primary Key & Identity Column
+            modelBuilder.Entity<Sensor>().HasKey(us => us.SensorId);
+            modelBuilder.Entity<Sensor>().Property(us => us.SensorId).IsRequired().HasColumnName("sensor_id");
+            //COLUMN SETTINGS 
+            modelBuilder.Entity<Sensor>().Property(us => us.SerialNumber).IsRequired(true).HasMaxLength(100).HasColumnName("serial_number");
+            modelBuilder.Entity<Sensor>().Property(us => us.Long).IsRequired(true).HasColumnName("longitude");
+            modelBuilder.Entity<Sensor>().Property(us => us.Lat).IsRequired(true).HasColumnName("latitude");
+            modelBuilder.Entity<Sensor>().Property(us => us.LastCommunication).IsRequired(true).HasColumnName("last_communication");
+            modelBuilder.Entity<Sensor>().Property(us => us.BatteryStatus).IsRequired(true).HasColumnName("battery_status");
+            modelBuilder.Entity<Sensor>().Property(us => us.OptimalGDD).IsRequired(true).HasColumnName("optimal_gdd");
+            modelBuilder.Entity<Sensor>().Property(us => us.CuttingDateTimeCalculated).IsRequired(true).HasColumnName("estimated_date");
+            modelBuilder.Entity<Sensor>().Property(us => us.LastForecastDate).IsRequired(true).HasColumnName("last_forecast_date");
+            modelBuilder.Entity<Sensor>().Property(us => us.State).IsRequired(true).HasConversion(
+            v => v.ToString(), v => (Enum)Enum.Parse(typeof(Enum), v)).HasMaxLength(250).HasColumnName("state");
+
+            modelBuilder.Entity<Sensor>().HasMany<User>(us => us.Users)
+                .WithMany(us => us.Sensors);
+
+
+            #endregion
+         
+            #region Notification
+            modelBuilder.Entity<Notification>().ToTable("notification");
+            //Primary Key & Identity Column
+            modelBuilder.Entity<Notification>().HasKey(us => us.NotificationId);
+            modelBuilder.Entity<Notification>().Property(us => us.NotificationId).IsRequired().HasColumnName("Notification_id");
+            //COLUMN SETTINGS 
+            modelBuilder.Entity<Notification>().Property(us => us.Title).IsRequired(true).HasMaxLength(100).HasColumnName("title");
+            modelBuilder.Entity<Notification>().Property(us => us.Message).IsRequired(true).HasMaxLength(200).HasColumnName("message");
+            modelBuilder.Entity<Notification>().Property(us => us.SentBy).IsRequired(true).HasConversion(
+            v => v.ToString(),v => (Enum)Enum.Parse(typeof(Enum), v)).HasColumnName("sent_by");
+            modelBuilder.Entity<Notification>().Property(us => us.Status).IsRequired(true).HasConversion(
+            v => v.ToString(),v => (Enum)Enum.Parse(typeof(Enum), v)).HasColumnName("status");
+         
+
+            #endregion
+
+
+
+        }
+    }
+}
---
+++ test/FarmAdvisor.Function.Test/DataAccess.MSSQLTest/CRUDTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using FarmAdvisor.DataAccess.MSSQL.DataContext;
+using FarmAdvisor.DataAccess.MSSQL.Entities;
+using FarmAdvisor.DataAccess.MSSQL.Functions.Crud;
+
+namespace FarmAdvisor.Function.Test.DataAcess.MSSQLTest
+{
+    public class CRUDTest
+    {
+        Mock<DatabaseContext> MockDbContext = new Mock<DatabaseContext>();
+
+
+
+        [Fact]
+        public async Task Positive_CreateUser()
+        {
+            var UserMock = new Mock<DbSet<User>>();
+            var User = new User
+            {
+                Name = "Test",
+                Email = "Test@test.com",
+                AuthId = "admin",
+                Phone = "1234567890"
+            };
+
+            UserMock.Setup(us => us.AddAsync(It.IsAny<User>(), It.IsAny<CancellationToken>()))
+                .Callback((User user, CancellationToken token) => { })
+                .Returns((User user, CancellationToken token) => { ValueTask.FromResult<User>(User); });
+            MockDbContext.Setup(us=>us.Set<User>()).Returns(UserMock.Object);
+            MockDbContext.Setup(us => us.SaveChangesAsync(It.IsAny<CancellationToken>())).Returns(() => Task.FromResult(1));
+
+            var UserCrud = new CRUD();
+            var result = await UserCrud.Create(User);
+
+            Assert.NotNull(result);
+        }
+
+    }
+}
