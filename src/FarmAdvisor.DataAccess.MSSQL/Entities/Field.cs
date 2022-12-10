82b81e4e9f972d1829bf3c94c6f56a0d9f7fa8bd
se348 <semir2578@gmail.com>
2022-12-11 00:31:06 +0300

    did fix bug

---
+++ src/FarmAdvisor.DataAccess.MSSQL/Entities/Field.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmAdvisor.DataAccess.MSSQL.Entities
+{
+    public class Field
+    {
+        public Guid FieldId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Alt { get; set; }
+        public string Polygon { get; set; }
+
+
+        public virtual Farm Farm { get; set; }  
+        public virtual ICollection<Sensor> Sensors { get; set; }
+    }
+}
