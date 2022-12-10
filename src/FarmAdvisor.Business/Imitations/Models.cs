b8272b878cb7358872915264c2f5c9f981468932
Stargazing-11 <samuelkifle199314@gmail.com>
2022-12-11 00:02:22 +0300

    made some changes

---
+++ src/FarmAdvisor.Business/Imitations/Models.cs
@@ -0,0 +1,34 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace FarmAdvisor.Business.Imitations
+{
+   
+        public class Weather : ITableEntity
+        {
+
+            public string PartitionKey { get; set; }
+            public string RowKey { get; set; }
+            public DateTimeOffset? Timestamp { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public ETag ETag { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public double AirPressureAtSeaLevel { get; set; }
+            public double CloudAreaFraction { get; set; }
+            public double RelativeHumidity { get; set; }
+            public double WindFromDirection { get; set; }
+            public double WindSpeed { get; set; }
+            public double Temperature { get; set; } 
+
+        }
+        public class CalculatedGDD : ITableEntity
+        {
+            public DateOnly ComputedTime { get; set; }
+            public int SensorId { get; set; }
+            public double Value { get; set; } = 0;
+            public string PartitionKey { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string RowKey { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public DateTimeOffset? Timestamp { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public ETag ETag { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        }
+
+}
