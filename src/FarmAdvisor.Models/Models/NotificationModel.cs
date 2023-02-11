487fd874aedd1f6c93a27a31c1811a9f6ec3e675
yosephhabtamu <yosephhabtamu5@gmail.com>
2023-02-11 18:13:05 +0300

    fixed enum

--- src/FarmAdvisor.Models/Models/NotificationModel.cs
+++ src/FarmAdvisor.Models/Models/NotificationModel.cs
@@ -1,4 +1,6 @@
-﻿namespace FarmAdvisor.Models.Models
+﻿using static FarmAdvisor.Models.Models.SensorModel;
+
+namespace FarmAdvisor.Models.Models
 {
 
     public class NotificationModel
@@ -20,7 +22,13 @@
         }
         public SenderEnum SentBy { get; set; }
 
-        public Enum? Status { get; set; }
+        public enum StatusEnum
+        {
+            Unknown,
+            Done,
+
+        }
+        public StatusEnum Status { get; set; }
 
         public FarmModel? Farm { get; set; }
 
