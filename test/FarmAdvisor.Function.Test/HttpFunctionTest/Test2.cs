e49655ebb1f9772f8895b901435bb429c432bf69
Dagim-Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-02-12 11:07:41 +0300

    test for farmendpoint

---
+++ test/FarmAdvisor.Function.Test/HttpFunctionTest/Test2.cs
@@ -0,0 +1,58 @@
+﻿using FarmAdvisor.Models.Models;
+using FarmAdvisor_HttpFunctions.Functions;
+using FarmAdvisor.DataAccess.MSSQL.Functions.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using FarmAdvisor_HttpFunctions.Functionsw;
+
+namespace FarmAdvisor_HttpFunctions.Tests
+{
+    public class FarmEndpointTests
+    {
+        [Fact]
+        public async Task GetFarmModel_ReturnsOkResult_WhenDataIsAvailable()
+        {
+            // Arrange
+            var farmId = Guid.NewGuid();
+            var farm = new FarmModel { FarmId = farmId, Name = "Test Farm", Postcode = "1234", City = "Test City", Country = "Test Country", UserId = Guid.NewGuid() };
+
+            var mockCrud = new Mock<ICrud>();
+            mockCrud.Setup(x => x.Find<FarmModel>(farmId)).ReturnsAsync(farm);
+
+            var farmEndpoint = new FarmEndpoint(mockCrud.Object);
+            var mockLogger = new Mock<ILogger>();
+
+            // Act
+            var result = await farmEndpoint.GetFarmModel(null, farmId, mockLogger.Object);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<FarmModel>(okResult.Value);
+            Assert.Equal(farm, returnValue);
+        }
+        [Fact]
+        public async Task GetFarmModel_ReturnsNotFoundResult_WhenDataIsNotAvailable()
+        {
+            // Arrange
+            var farmId = Guid.NewGuid();
+
+            var mockCrud = new Mock<ICrud>();
+            mockCrud.Setup(x => x.Find<FarmModel>(farmId)).ThrowsAsync(new Exception("Data not found"));
+
+            var farmEndpoint = new FarmEndpoint(mockCrud.Object);
+            var mockLogger = new Mock<ILogger>();
+
+            // Act
+            var result = await farmEndpoint.GetFarmModel(null, farmId, mockLogger.Object);
+
+            // Assert
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal("Data not found", notFoundResult.Value);
+        }
+
+    }
+}
