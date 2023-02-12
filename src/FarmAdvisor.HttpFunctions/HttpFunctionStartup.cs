8561edcac78c4b0dbcaafc80ab319b5c04920549
se348 <semir2578@gmail.com>
2023-02-12 09:50:51 +0300

    did merge sensor endpoint

--- src/FarmAdvisor.HttpFunctions/HttpFunctionStartup.cs
+++ src/FarmAdvisor.HttpFunctions/HttpFunctionStartup.cs
@@ -1,9 +1,10 @@
 using FarmAdvisor.DataAccess.AzureTableStorage.services;
-using FarmAdvisor.DataAccess.MSSQL.Functions.Crud;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+
 [assembly: FunctionsStartup(typeof(FarmAdvisor.HttpFunctions.HttpFunctionStartup))]
 
 namespace FarmAdvisor.HttpFunctions
@@ -12,8 +13,9 @@ namespace FarmAdvisor.HttpFunctions
     {
         public static void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICrud, Crud>();
             services.AddScoped<ITableStorageService, TableStorageService>();
-            services.AddScoped<DataAccess.MSSQL.Functions.Interfaces.ICrud, Crud>();
+            services.AddTransient<ICrud, Crud>();
         }
 
 
