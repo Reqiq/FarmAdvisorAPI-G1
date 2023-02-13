83c715dd0a3ad1a936822bd121ea2236d72316a9
yosephhabtamu <yosephhabtamu5@gmail.com>
2023-02-13 19:42:14 +0300

    test Debugging

--- test/FarmAdvisor.DataAccess.AzureTableStorage.Test/TableStorageServiceTests.cs
+++ test/FarmAdvisor.DataAccess.AzureTableStorage.Test/TableStorageServiceTests.cs
@@ -1,4 +1,4 @@
-using System;
+/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -75,4 +75,4 @@ namespace FarmAdvisor.Function.Test.DataAccess.AzureTableStorage
             // Assert.Null(removed);
         }
     }
-}
+}*/
